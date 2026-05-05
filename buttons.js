9a9d28c014fae2323793fd83d90d48bf21c797e2
Kaendas <lauri.julku@gmail.com>
2026-05-05 12:40:10 +0300

    kirjotus virhe

--- buttons.js
+++ buttons.js
@@ -55,7 +55,7 @@ const imagesToPreloadMobile = [
   'kuvat/serlachius-black.png',
   'kuvat/KysymysTausta3.png',
   'kertoja_FI.png',
-  'kuvat/kertoja_en.png',
+  'kuvat/Kertoja_en.png',
 ];
 const imagesToPreloadDesktop =[
   'kuvat/kartta_1.png',
